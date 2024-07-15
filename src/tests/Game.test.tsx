fcf7b1fb873fe887fc31809d4522eee16dc01cb9
Radenavarro <rauldelapaznavarro@gmail.com>
2024-07-15 10:38:57 +0200

    eliminado test INUTIL

--- src/tests/Game.test.tsx
+++ src/tests/Game.test.tsx
@@ -1,8 +1,7 @@
-import { render, screen, fireEvent } from '@testing-library/react'
-import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import { beforeEach, describe, expect, test } from 'vitest'
 import { PlayerLayer } from '../views/layers/Player'
 import Game from '../views/Game'
-import { act } from 'react'
 
 describe('Game', () => {
   beforeEach(() => {
@@ -22,33 +21,8 @@ describe('PlayerLayer', () => {
     container = renderResult.container
   })
 
-  afterEach(() => {
-    vi.restoreAllMocks()
-    vi.useRealTimers()
-  })
-
   test('Is visible', () => {
     const canvas = container.querySelector('canvas')
     expect(canvas).toBeDefined()
-  })
-
-  test('Movement works', async () => {
-    const canvas = container.querySelector('canvas')
-    const ctx = canvas?.getContext('2d')
-
-    let initialImageData:ImageData | boolean = false
-    let updatedImageData:ImageData | boolean = false
-    if (ctx) {
-      initialImageData = ctx.getImageData(0, 0, 800, 600)
-      console.log(initialImageData)
-
-      await act(async () => {
-        fireEvent.keyDown(document, { key: 'd' })
-      })
-
-      updatedImageData = ctx.getImageData(0, 0, 800, 600)
-    }
-
-    expect(initialImageData).not.toBe(updatedImageData)
   })
 })
